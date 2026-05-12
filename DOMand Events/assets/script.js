9d9fe6926ac9dcbad3e0661c87361f94cd358293
wangarigathiru <wangarigathiru@gmail.com>
2026-05-12 13:22:19 +0300

    javascript learning

---
+++ DOMand Events/assets/script.js
@@ -0,0 +1,5 @@
+//get the element to manipulate.
+document.getElementsByClassName("DOM_Header")[0].innerHTML = "Iam learning DOM and Events";
+//changing the text.
+document.getElementsByClassName("DOM_Header")[0].innerHTML = "<h1>Hello, World!</h1>";
+document.getElementById("firstparagraph").textContent = "Welcome to DOM";
